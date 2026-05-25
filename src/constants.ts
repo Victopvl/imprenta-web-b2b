b8317121c32aaf087308588cea7ffd925396c78e
Victopvl <victoriapaz.sa@gmail.com>
2026-05-25 22:41:46 +0000

    add contac

--- src/constants.ts
+++ src/constants.ts
@@ -2,8 +2,8 @@
 
 export const CONTACT_INFO = {
   companyName: "VataGraphic",
-  phone: "+56912345678",
-  email: "ejecutivo@tuimprenta.com",
-  whatsappUrl: "https://wa.me/56912345678?text=Hola,%20vengo%20de%20la%20página%20web...",
+  phone: "+56998651656",
+  email: "mvallejos@vatagraphic.cl",
+  whatsappUrl: "https://wa.me/56998651656?text=Hola,%20vengo%20de%20la%20página%20web...",
   address: "Santiago, Chile",
 };
