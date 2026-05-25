3583bcb27514bbf231360fd3f7c37d10c995ac3b
Victopvl <victoriapaz.sa@gmail.com>
2026-05-25 23:10:34 +0000

    Configuración de GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,12 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  // 1. Reemplaza con tu nombre de usuario de GitHub
+  site: 'https://victopvl.github.io',
+  
+  // 2. Reemplaza con el nombre exacto de tu repositorio (mantén las barras / adelante y atrás)
+  base: '/imprenta-web-b2b/',
+
   vite: {
     plugins: [tailwindcss()]
   }
