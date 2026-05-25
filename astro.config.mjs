eca4fc5e700fdc60b99724bc714161deb37b51e8
Victopvl <victoriapaz.sa@gmail.com>
2026-05-25 23:15:21 +0000

    Fix: correccion de empaquetado para github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  // 1. Reemplaza con tu nombre de usuario de GitHub
-  site: 'https://victopvl.github.io',
-  
-  // 2. Reemplaza con el nombre exacto de tu repositorio (mantén las barras / adelante y atrás)
+  site: 'https://Victopvl.github.io',
   base: '/imprenta-web-b2b/',
-
   vite: {
     plugins: [tailwindcss()]
   }
