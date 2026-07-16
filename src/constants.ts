86b822910faf3e7280b76c566f49b2661d727c5a
Victopvl <victoriapaz.sa@gmail.com>
2026-07-16 20:26:02 +0000

    c

--- src/constants.ts
+++ src/constants.ts
@@ -8,5 +8,8 @@ export const CONTACT_INFO = {
   address: "Santiago, Chile",
 };
 
-// TODO: Reemplazar por tu access key real de Web3Forms (gratis en https://web3forms.com)
+// Tu clave real de Web3Forms (¡Ya está configurada!)
 export const WEB3FORMS_ACCESS_KEY = "4fdda8c2-f69f-4cbb-9129-bb877279585e";
+
+// Tu URL de Google Apps Script (¡Pega aquí la que obtengas al implementar en el Paso 4!)
+export const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxFLwtPawh51FYtRitjqgbKIld-yEvSnmRHMeKCLbokrXX2NaNAiD_V2_5q9hCD0biD/exec";
